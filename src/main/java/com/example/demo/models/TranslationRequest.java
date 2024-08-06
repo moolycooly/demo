7e4e8aed19122ae36befce91cf2410775ca56fcb
Sergey T <moolycooly2004@gmail.com>
2024-08-06 19:58:13 +0300

    Commit

--- src/main/java/com/example/demo/models/TranslationRequest.java
+++ src/main/java/com/example/demo/models/TranslationRequest.java
@@ -9,14 +9,22 @@ public class TranslationRequest {
     private String text;
     private String source;
     private String target;
-
     private String ipAddress;
 
+    public TranslationRequest() {
+
+    }
     public TranslationRequest(String text, String source, String target) {
         this.text = text;
         this.source = source;
         this.target = target;
     }
 
+    public TranslationRequest(String text, String source, String target, String ipAddress) {
+        this.text = text;
+        this.source = source;
+        this.target = target;
+        this.ipAddress = ipAddress;
+    }
 }
 
--- src/main/java/com/example/demo/services/TranslationService.java
+++ src/main/java/com/example/demo/services/TranslationService.java
@@ -6,7 +6,6 @@ import com.example.demo.repositories.TranslationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
 
@@ -34,6 +33,4 @@ public class TranslationService {
         return result;
 
     }
-
-
 }
