2515f0d6d6d31d463b398fbda19b67c4c68cd995
Sergey T <moolycooly2004@gmail.com>
2024-08-06 22:19:12 +0300

    Commit

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS translation_requests (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     ip_address VARCHAR(255) NOT NULL,
     original_text TEXT NOT NULL,
     translated_text TEXT NOT NULL
